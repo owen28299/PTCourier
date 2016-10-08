1ab426b0fff9f024056399eca08aad5ee88670a5
meghankradolfer <meghan@woof.com.au>
2016-10-08 21:40:30 +1100

    meghan: styling

--- react-src/components/home.js
+++ react-src/components/home.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { browserHistory } from 'react-router';
 
+
+
 const GetAll = React.createClass({
   render : function(){
     return (
-      <div className="all-jobs">
-        <h1>Welcome to zap</h1>
+      <div className="home">
+          <div className="container">
+             <div className="row col-md-6 col-md-offset-3">
+                 <h1>Welcome to Zap!</h1>
+                 <h4>Are you a...</h4>
+                 <button className="btn btn-default">Custumer</button>
+                 <button className="btn btn-default">Courier</button>
+             </div>
+          </div>
       </div>
     )
   }
