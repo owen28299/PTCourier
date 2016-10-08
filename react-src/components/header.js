ff041d901192c8fc3c7ed6e5fee07ba57a3867b8
meghankradolfer <meghan@woof.com.au>
2016-10-09 00:23:51 +1100

    meghan: homepage

--- react-src/components/header.js
+++ react-src/components/header.js
@@ -10,15 +10,15 @@ import Logo from './partials/images.js';
 const Header = React.createClass({
     renderHeader: function() {
         const path = this.props.location.pathname;
-        if (path == '/client') {
+        if (path.indexOf('/client') != -1) {
             return (
                 <Nav pullRight>
                     <LinkContainer to="/">
-                        <NavItem eventKey={1} >Log Out</NavItem>
+                        <NavItem eventKey={1} >Client Log Out</NavItem>
                     </LinkContainer>
                 </Nav>
             );
-        } else if (path === '/courier') {
+        } else if (path.indexOf('/courier') != -1) {
             return (
                 <Nav pullRight>
                     <LinkContainer to="/">
--- react-src/components/home.js
+++ react-src/components/home.js
@@ -7,13 +7,20 @@ const GetAll = React.createClass({
   render : function(){
     return (
       <div className="home">
-          <div className="container">
-             <div className="row col-md-6 col-md-offset-3">
-                 <h1>Welcome to Zap!</h1>
-                 <h4>Are you a...</h4>
-                 <button className="btn btn-default">Custumer</button>
-                 <button className="btn btn-default">Courier</button>
-             </div>
+          <div className="parallax-bg" style={{backgroundImage: 'url(\'./theme/assets/images/melb_bg.jpg\')'}}>
+              <div className="container">
+                  <div className="row title">
+                      <div className="col-md-7">
+                          <h1>Welcome to Zap!</h1>
+                          <h4>Some tag line</h4>
+                      </div>
+                      <div className="col-md-5">
+                          <h4>Would you like to...</h4>
+                          <button className="btn btn-primary">Zap an urgent delivery</button><br />
+                          <button className="btn btn-primary">Deliver urgent goods</button>
+                      </div>
+                  </div>
+              </div>
           </div>
       </div>
     )
