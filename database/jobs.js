a600b9c66007ffa3e0e32846313705cd049b18e5
Owen <owenyangg@gmail.com>
2016-10-08 19:01:30 +1100

    Basic interface set up

--- database/jobs.js
+++ database/jobs.js
@@ -1,3 +1,27 @@
 module.exports = {
-  jobs : []
+  jobs : [
+    {
+      id : 0,
+      client : "Mark Smith",
+      item : "A4 Batteries",
+      item_location : "JB Hifi",
+      delivery_location : "Melbourne Convention Centre",
+      time : 3,
+      budget : 50,
+      applicants : [
+        {
+          id : 0,
+          name : "Tony Stark",
+          offer : 50,
+          time : 2.5
+        },
+        {
+          id : 1,
+          name : "Mickey Mouse",
+          offer : 35,
+          time : 4
+        }
+      ]
+    }
+  ]
 };
