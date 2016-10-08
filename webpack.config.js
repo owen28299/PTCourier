dbd8da0864395479bc1e031e661b71451fe89cc2
meghankradolfer <meghan@woof.com.au>
2016-10-08 16:52:56 +1100

    meghan: hot loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,16 @@
+var webpack = require('webpack');
 module.exports = {
-  entry: './react-src/index.js',
-  output: {
-    filename: './public/js/browser-bundle.js'
-  },
-  devtool: 'source-map',
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react']
-        }
-      },
-    ]
-  }
+    entry: './react-src/index.js',
+    output: {
+        filename: './public/js/browser-bundle.js'
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
 };
